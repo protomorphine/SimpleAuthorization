39b3031479574d0aa60c83227cb94f0bed4e1a99
Daniil <kupernewfag@gmail.com>
2022-09-22 13:49:46 +0300

    added auth attribute

---
+++ SimpleAuthorization.API/Attributes/AuthReqiredAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace SimpleAuthorization.API.Attributes
+{
+    public class AuthReqiredAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly IMemoryCache _cache;
+
+        public AuthReqiredAttribute(IMemoryCache cache) => _cache = cache;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (_cache.Get(context.HttpContext.Request.Cookies["auth"]) == null)
+                context.Result = new UnauthorizedResult();
+            //throw new UnauthorizedAccessException("Unauthorized.");
+        }
+    }
+}
