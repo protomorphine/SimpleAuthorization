ce1cf5990425494fcd761b252d64d8c1c3506c53
Daniil <kupernewfag@gmail.com>
2022-09-30 15:36:17 +0300

    rename dto

---
+++ SimpleAuthorization.Core/Dtos/CreateAndUpdateUserDto.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleAuthorization.Core.Dtos;
+
+/// <summary>
+/// Дто создания нового пользователя
+/// </summary>
+public class CreateAndUpdateUserDto
+{
+    /// <summary>
+    /// Логин пользователя
+    /// </summary>
+    public string? Login { get; set; }
+
+    /// <summary>
+    /// Пароль пользователя
+    /// </summary>
+    public string? Password { get; set; }
+
+    /// <summary>
+    /// Фио пользоваетля
+    /// </summary>
+    public string? Fio { get; set; }
+
+    /// <summary>
+    /// Идентификатор организации пользователя
+    /// </summary>
+    public long? OrganizationId { get; set; }
+}
