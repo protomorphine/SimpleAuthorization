794b5538f0855e0ef01f2882361a123af28e0cad
Daniil <kupernewfag@gmail.com>
2022-10-03 08:23:13 +0300

    added role check middleware

---
+++ SimpleAuthorization.API/Models/EndpointModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleAuthorization.API.Models;
+
+/// <summary>
+/// Модель эндпоинта
+/// </summary>
+public class EndpointModel
+{
+    /// <summary>
+    /// Имя контроллера
+    /// </summary>
+    public string Controller { get; set; }
+
+    /// <summary>
+    /// Имя метода
+    /// </summary>
+    public string Action { get; set; }
+
+    /// <summary>
+    /// HTTP метод
+    /// </summary>
+    public string? Method { get; set; }
+}
