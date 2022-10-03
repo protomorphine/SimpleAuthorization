91d5d8d54497ed5690f17554eae692ec63863796
Daniil <kupernewfag@gmail.com>
2022-10-03 09:17:34 +0300

    added maping to 403 status code

--- SimpleAuthorization.API/Extensions/ProblemDetailsExtensions.cs
+++ SimpleAuthorization.API/Extensions/ProblemDetailsExtensions.cs
@@ -4,12 +4,27 @@ using SimpleAuthorization.Core.Exceptions;
 
 namespace SimpleAuthorization.API.Extensions;
 
+/// <summary>
+/// Расширения для конфигурации <see cref="ProblemDetailsMiddleware"/>
+/// </summary>
 public static class ProblemDetailsExtensions
 {
+    /// <summary>
+    /// Мапинг эксепшенов в коде на стандартизированну ошибку
+    /// </summary>
+    /// <param name="options"></param>
     public static void ConfigureProblemDetails(ProblemDetailsOptions options)
     {
-        options.Map<UnauthorizedException>(ex => new ExtendedExceptionProblemDetails(ex, StatusCodes.Status401Unauthorized));
-        options.Map<ObjectNotFoundException>(ex => new ExtendedExceptionProblemDetails(ex, StatusCodes.Status404NotFound));
-        options.Map<UserAlreadyExistException>(ex => new ExtendedExceptionProblemDetails(ex, StatusCodes.Status400BadRequest));
+        options.Map<UnauthorizedException>(ex => 
+            new ExtendedExceptionProblemDetails(ex, StatusCodes.Status401Unauthorized));
+        
+        options.Map<ObjectNotFoundException>(ex => 
+            new ExtendedExceptionProblemDetails(ex, StatusCodes.Status404NotFound));
+        
+        options.Map<UserAlreadyExistException>(ex => 
+            new ExtendedExceptionProblemDetails(ex, StatusCodes.Status400BadRequest));
+        
+        options.Map<InvalidOperationException>(ex =>
+            new ExtendedExceptionProblemDetails(ex, StatusCodes.Status403Forbidden));
     }
 }
